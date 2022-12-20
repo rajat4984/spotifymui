866aba51c0d5579c318b32b8d170ec258957a4d1
Rajat <rs20021023@gmail.com>
2022-12-20 18:03:01 +0530

    delete request get request and post request completed

--- backend/public/app.js
+++ backend/public/app.js
@@ -1,8 +1,87 @@
 const submitBtn = document.querySelector(".submit-btn");
-submitBtn.addEventListener("click", async (e) => {
+const nameInput = document.querySelector(".name-input");
+const weightInput = document.querySelector(".weight-input");
+const priceInput = document.querySelector(".price-input");
+const listItems = document.querySelector(".list-items");
+
+const checkValue = ()=>{
+  if (nameInput.value === "" || weightInput.value ==="" || priceInput.value === ""){
+    submitBtn.disabled = true;
+  }
+  else{
+    submitBtn.disabled = false;
+  }
+}
+
+submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const response = await fetch("http://localhost:5000/newEq");
-  // console.log(response);
-  const data = await response.json();
-  console.log(data);
+  postRequest();
+  getRequest();
 });
+window.addEventListener('load', () => {
+  getRequest();
+});
+
+const postRequest = async () => {
+  try {
+    const postResponse = await fetch("/newEq", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: nameInput.value,
+        weight: weightInput.value,
+        price: priceInput.value,
+      }),
+    });
+    nameInput.value = "";
+    weightInput.value = "";
+    priceInput.value = "";
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getRequest = async () => {
+  try {
+    const getResponse = await fetch("/allEq", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await getResponse.json();
+    updateList(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteEq = async (e) => {
+  const id = e.target.id;
+  try {
+    const deleteReq = await fetch("/deleteEq" + "/" + id, {
+      method: "DELETE",
+    });
+    getRequest();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateList = (data) => {
+  data = data.eq;
+  listItems.innerHTML = "";
+
+  data.map((eq, index) => {
+    listItems.innerHTML += `<div class="list-item">
+    <div class="number">${index+1}.</div>
+    <div class="eq-name">${eq.name}</div>
+    <div class="eq-weight">${eq.weight}Kg</div>
+    <div class="eq-price">$${eq.price}</div>
+    <div><i class="bi bi-trash" class="eq-delete" id="${eq._id}" onclick="deleteEq(event)"></i></div>
+  </div>
+</div>`;
+  });
+};
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,36 @@
 const express = require("express");
 const connectDB = require("./db/connectDB");
 const app = express();
-const MONGO_URI =
-  "mongodb+srv://rajat4984:rajat4984@nodeexpressproject.zv05z.mongodb.net/?retryWrites=true&w=majority";
-const port = 5000;
-app.use(express.static("./public"));
-app.use(express.json());
+const Eq = require("./modals/equipments");
+const cors = require("cors");
 
-app.get("/newEq", (req, res) => {
-  res.json({
-    user: "user1",
-  });
+const MONGO_URI =
+  "mongodb+srv://rajat4984:rajat4984@nodeexpressproject.zv05z.mongodb.net/Gym?retryWrites=true&w=majority";
+const port = 5000;
+
+app.use(express.json());
+app.use(express.static("./public"));
+app.use(cors());
+
+app.get("/allEq", async (req, res) => {
+  console.log("all eq hit");
+  const eq = await Eq.find({});
+  res.status(200).send({ eq });
+});
+
+app.post("/newEq", async (req, res) => {
+  const eq = await Eq.create(req.body);
+  res.status(204).send({ eq });
+});
+
+app.delete("/deleteEq/:id", async (req, res) => {
+  const id = req.params.id;
+  const eq = await Eq.findByIdAndRemove({ _id: id });
+  if (!eq) {
+    throw new NotFoundError(`No job with id ${id}`);
+  }
+  res.status(200).send({eq});
+  console.log(eq);
 });
 
 const start = async () => {
