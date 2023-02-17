2f2f0843a747813bc02035448d058267c7e7120a
Rajat sharam <rs20021023@gmail.com>
2023-02-17 22:23:16 +0530

    sidebar completed

---
+++ spotifymui/src/App.js
@@ -0,0 +1,30 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { Box, Stack } from "@mui/system";
+import React from "react";
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  const myTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+    typography: {
+      fontSize: 12,
+    },
+  });
+  return (
+    <Box bgcolor="backgrund.default">
+      <ThemeProvider theme={myTheme}>
+        <CssBaseline />
+        <Stack direction="row">
+          <Sidebar />
+          <Feed />
+        </Stack>
+      </ThemeProvider>
+    </Box>
+  );
+}
+
+export default App;
---
+++ spotifymui/src/components/Feed.js
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+function Feed() {
+  return (
+    <Box flex={5}>
+      Feed
+    </Box>
+  );
+}
+
+export default Feed;
